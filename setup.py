2711589ed34fd0b38b568fbcc333b5f9cf6ae652
cms66 <58144229+cms66@users.noreply.github.com>
2024-12-29 19:22:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 # First boot - Base setup
 # wget https://raw.githubusercontent.com/cms66/rpi-pycluster/main/setup.py; sudo python ./setup.py
 
+# Imports
+import  subprocess, sys, os, urllib.request
+
+def main():
+    print("Python setup")
+    print("Python setup done")
+    
+if __name__ == "__main__":
+    main()
