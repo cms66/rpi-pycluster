2cabf9f92090956abc0125785ee26aebc500fc61
cms66 <58144229+cms66@users.noreply.github.com>
2025-01-07 13:42:33 +0000

    Create setup_hardware.sh

---
+++ setup_hardware.sh
@@ -0,0 +1,16 @@
+# Hardware setup functions
+
+setup_camera_csi()
+{
+	read -p "Function not yet available, press enter to continue"
+}
+
+setup_camera_usb()
+{
+	read -p "Function not yet available, press enter to continue"
+}
+
+setup_sense_hat()
+{
+	read -p "Function not yet available, press enter to continue"
+}
