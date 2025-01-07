3bce50457a6e755718e5d4d5a1b9ade57bfe125f
cms66 <58144229+cms66@users.noreply.github.com>
2025-01-07 19:20:49 +0000

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -23,7 +23,7 @@ update_system_base()
 	printf "%s\n" "Updating system"
 	apt-get -y update
 	apt-get -y upgrade
-	apt-get -y install python3-dev gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress-ng zip bzip2 fail2ban ufw ntpdate pkgconf openssl
+	apt-get -y install python3-dev gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress-ng zip bzip2 fail2ban ufw ntpdate pkgconf openssl libmunge-dev munge
  	# Remove local SDM
   	rm -rf /usr/local/sdm
  	rm -rf /usr/local/bin/sdm
@@ -36,27 +36,6 @@ setup_ntp()
 	printf "%s\n" "Configuring ntp"
  	sed -i "s/#FallbackNTP/FallbackNTP/g" /etc/systemd/timesyncd.conf # Setup NTP
   printf "%s\n" "ntp setup complete"
-}
-
-install_munge_local()
-{
-	# Create System group and user
-	# Check uid/guid/user available
-	defid=991
-	getent passwd $defid && mnguid=$? # 0 = uid exists
-	getent group $defid && mnggid=$? # 0 = guid exists
-	getent passwd munge && mngusr=$? # 0 = uid exists
-	if [[ $mnguid ]] || [[ $mnggid ]] || [[ $mngusr ]]; then # uid/guid/user exists
-		read -p "UID, GUID or user exists"
-		return
-	else
-		#read -p "UID and GUID available"
-		groupadd -r -g $defid munge
-		useradd -r -c "MUNGE Uid 'N' " -g munge -u $defid -d /var/lib/munge -s /sbin/nologin munge
-		apt-get -y install libmunge-dev munge
-		
-	fi
-    read -p "Munge install done"
 }
 
 # Git setup
@@ -146,7 +125,6 @@ get_subnet_cidr()
 
 update_system_base
 setup_ntp
-install_munge_local
 setup_git
 create_venv
 setup_fail2ban
