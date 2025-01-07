166d8845230c808e7d2cb8c7f2a9bb3abaae81c0
cms66 <58144229+cms66@users.noreply.github.com>
2025-01-07 12:31:00 +0000

    Update setup_main.sh

--- setup_main.sh
+++ setup_main.sh
@@ -24,7 +24,7 @@ pimodel=$(cat /sys/firmware/devicetree/base/model)
 pimodelnum=$(echo $pimodel | cut -d " " -f 3)
 pirev=$(cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//')
 osarch=$(getconf LONG_BIT)
-repo="rpi-pyhome"
+repo="rpi-pycluster"
 dirscr=$PWD # Directory of calling script
 
 show_menu mnuMainFull # Show main menu
