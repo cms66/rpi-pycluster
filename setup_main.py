1a952df56e9d976e1c267fb0d637dd33e791a2ed
cms66 <58144229+cms66@users.noreply.github.com>
2025-01-07 12:08:04 +0000

    Update setup_main.py

--- setup_main.py
+++ setup_main.py
@@ -1,1 +1,11 @@
+# Entry point for python based management system
 
+# Imports
+import subprocess, sys
+
+def main():
+    print("Main setup")
+    print (sys.argv)
+
+if __name__ == "__main__":
+    main()
