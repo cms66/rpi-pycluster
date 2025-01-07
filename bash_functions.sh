e57798302ca4bd80a432479d4dced3301450004f
cms66 <58144229+cms66@users.noreply.github.com>
2025-01-07 11:40:09 +0000

    Update bash_functions.sh

--- bash_functions.sh
+++ bash_functions.sh
@@ -10,6 +10,13 @@ handle_error()
 # Set the error handler to be called when an error occurs
 trap handle_error ERR
 
+# Variables
+usrname=$(logname) # Script runs as root
+piname=$(hostname)
+repo="rpi-pycluster"
+repobranch="main"
+pimodelnum=$(cat /sys/firmware/devicetree/base/model | cut -d " " -f 3)
+
 # Install/update software
 update_system_base()
 {
@@ -52,6 +59,96 @@ install_munge_local()
     read -p "Munge install done"
 }
 
+# Git setup
+setup_git()
+{
+	printf "%s\n" "Setting up Git"
+	mkdir /home/$usrname/.pisetup
+	cd /home/$usrname/.pisetup
+	git clone https://github.com/cms66/$repo.git
+	printf "# Setup - Custom configuration\n# --------------------\n\
+	repo = $repo\n\
+	repobranch = $repobranch\n" > /home/$usrname/.pisetup/custom.conf
+	chown -R $usrname:$usrname /home/$usrname/.pisetup
+	# Add bash alias for setup and test menu
+	echo "alias mysetup=\"sudo bash ~/.pisetup/$repo/setup_main.sh\"" >> /home/$usrname/.bashrc
+	echo "alias mytest=\"sudo bash ~/.pisetup/$repo/test_main.sh\"" >> /home/$usrname/.bashrc
+	echo "alias pysetup=\"sudo python ~/.pisetup/$repo/setup_main.py\"" >> /home/$usrname/.bashrc 	
+	echo "alias pytest=\"sudo python ~/.pisetup/$repo/test_main.py\"" >> /home/$usrname/.bashrc
+}
+
+# - Create python Virtual Environment (with access to system level packages) and bash alias for activation
+create_venv()
+{
+	printf "%s\n" "Creating python Virtual Environment"
+	python -m venv --system-site-packages /home/$usrname/.venv
+	echo "alias myvp=\"source ~/.venv/bin/activate\"" >> /home/$usrname/.bashrc
+	echo "alias dvp=\"deactivate\"" >> /home/$usrname/.bashrc
+	chown -R $usrname:$usrname /home/$usrname/.venv
+}
+
+# Configure fail2ban
+setup_fail2ban()
+{
+	printf "%s\n" "Configuring fail2ban"
+	cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+	cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+ 	# Setup ssh rules
+	strssh="filter	= sshd\n\
+banaction = iptables-multiport\n\
+bantime = -1\n\
+maxretry = 3\n\
+findtime = 24h\n\
+backend = systemd\n\
+journalmatch = _SYSTEMD_UNIT=ssh.service + _COMM=sshd\n\
+enabled = true\n"
+	sed -i "s/backend = %(sshd_backend)s/$strssh/g" /etc/fail2ban/jail.local
+}
+
+# Configure firewall (ufw)
+setup_firewall()
+{
+	printf "%s\n" "Configuring firewall"
+	# Allow SSH from local subnet only, unless remote access needed
+	read -rp "Allow remote ssh acces (y/n): " inp </dev/tty
+	if [ X$inp = X"y" ] # TODO - not case insensitive
+	then # Remote
+ 		yes | sudo ufw allow ssh
+	else # Local
+		yes | sudo ufw allow from $localnet to any port ssh
+	fi
+	sudo ufw logging on
+	yes | sudo ufw enable
+}
+
+get_subnet_cidr()
+{
+	wired="$(nmcli -t connection show --active | grep ethernet | cut -f 4 -d ":")"
+	wifi="$(nmcli -t connection show --active | grep wireless | cut -f 4 -d ":")"
+ 	if [[ $wifi ]] && [[ $wired ]] # Multiple connections
+	then
+		read -p "Use ethernet or wifi for setup? (e/w): " inp
+		if [[ ${inp,} = "e" ]]
+		then
+			dev=$wired
+		elif [[ ${inp,} = "w" ]]
+		then
+			dev=$wifi
+		else
+			printf "invalid option"
+		fi
+	else # Single connection
+		dev="$wifi$wired" 
+	fi
+ 	export localnet=$(nmcli -t device show $dev | grep "ROUTE\[1\]" | cut -f 2 -d "=" | tr -d '[:blank:]' | sed "s/,nh//")
+	printf "Device = $dev | localnet = $localnet\n"
+}
+
 update_system_base
 setup_ntp
 install_munge_local
+setup_git
+create_venv
+setup_fail2ban
+get_subnet_cidr
+setup_firewall
