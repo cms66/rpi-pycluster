e4e73c815288a0e79298c4e0117cadfed57e17d1
cms66 <58144229+cms66@users.noreply.github.com>
2025-01-09 22:27:09 +0000

    Update setup_hardware.sh

--- setup_hardware.sh
+++ setup_hardware.sh
@@ -12,5 +12,15 @@ setup_camera_usb()
 
 setup_sense_hat()
 {
-	read -p "Function not yet available, press enter to continue"
+	apt-get -y install sense-hat
+	sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot/config.txt
+ 	# Install cli calibration
+	wget -O RTIMULib.zip https://github.com/RPi-Distro/RTIMULib/archive/master.zip
+	unzip RTIMULib.zip
+	cd RTIMULib-master/Linux/RTIMULibCal
+	make
+	make install
+	cd /home/$usrname
+	rm -rf RTIMULib*
+ 	read -p Sense-hat setup complete, press enter to continue"
 }
